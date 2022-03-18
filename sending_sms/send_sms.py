bca129ea56170bd02d7717e7550287316366fccd
BrandonHdz27 <brndon.hernandez@latimes.com>
2022-03-17 21:29:37 -0600

    refactoring auth credentials

--- sending_sms/send_sms.py
+++ sending_sms/send_sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'AC73e613f70e11a5c627654fc2da655da6' 
-auth_token = 'db937aa815c734e7e3596f26b3515949' 
+account_sid = '' 
+auth_token = '' 
 client = Client(account_sid, auth_token) 
 
 message = client.messages.create(  
-        messaging_service_sid='MG18711c91c6dcf32f84248553137e2cd8', 
+        messaging_service_sid='', # messaging service sid 
         body='this actually works!',      
-        to='+525628253359' 
+        to='' # +521234567890
     )
 
 print(message.sid)
