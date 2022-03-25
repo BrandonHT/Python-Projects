364676912283d821faa78d4ea0e87ee9401c2f16
BrandonHdz27 <brndon.hernandez@latimes.com>
2022-03-25 01:54:21 -0600

    first flask server

---
+++ web_development/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
+
+@app.route("/")
+def about():
+    return render_template('./about.html')
